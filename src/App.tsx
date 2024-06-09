d55c025b9f17a7c1d6a77c67141e4fec60e3d33b
Martin Wheeler <martin@martinwheeler.com.au>
2024-06-09 16:26:01 +1000

    feat: basic shooting mechanics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
 
 type Player = {
@@ -13,20 +20,39 @@ type Movement = {
   //// track the keys pressed
 };
 
-const MOVEMENT_DELAY = 10; // milliseconds
+type BulletPosition = {
+  x: number;
+  y: number;
+};
 
-function Player() {
+type Bullet = {
+  spawn: BulletPosition;
+  mouseHeading: BulletPosition;
+  id: string;
+};
+
+const INTERVAL_DELAY = 50; // milliseconds
+
+function Player({
+  setBullets,
+}: {
+  setBullets: Dispatch<SetStateAction<Bullet[]>>;
+}) {
   const [player, setPlayer] = useState<Player>({
     position: {
       x: 0,
       y: 0,
     },
     health: 100,
+    // TODO: Buffs
+    // TODO: Ammo (bullets, grenades)
+    // TODO:
   });
 
   const [keysPressed, setKeysPressed] = useState<string[]>([]);
 
   const playerMovementInterval = useRef<number>(0);
+  const shootingInterval = useRef<number>(0);
 
   // DONE: 1. Capture the users onKeyDown event for a key press & then keep triggering the key until we get an onKeyUp event
   // this is to simulate multiple key presses as we can't handle multiple out of the box
@@ -39,15 +65,15 @@ function Player() {
   //
 
   // Where do we put a loop/tick engine? We want to trigger the key every 100 milliseconds
-
   useEffect(() => {
     window.addEventListener("keydown", handleAddKey);
     window.addEventListener("keyup", handleRemoveKey);
 
     playerMovementInterval.current = setInterval(
       handlePlayerMovement,
-      MOVEMENT_DELAY,
+      INTERVAL_DELAY
     );
+    shootingInterval.current = setInterval(handleShooting, INTERVAL_DELAY);
 
     return () => {
       window.removeEventListener("keydown", handleAddKey);
@@ -55,6 +81,8 @@ function Player() {
 
       if (playerMovementInterval.current)
         clearInterval(playerMovementInterval.current);
+
+      if (shootingInterval.current) clearInterval(shootingInterval.current);
     };
   });
 
@@ -62,8 +90,8 @@ function Player() {
   // NOTE: The max limit will be relative to the window size
   function handlePlayerMovement() {
     keysPressed.forEach((currentKey) => {
-      if (currentKey === "w") {
-        // up
+      // up
+      if (currentKey === "KeyW") {
         console.log("up");
 
         setPlayer((currentPlayer) => ({
@@ -75,8 +103,8 @@ function Player() {
         }));
       }
 
-      if (currentKey === "r") {
-        // down
+      // down
+      if (currentKey === "KeyS") {
         console.log("down");
         setPlayer((currentPlayer) => ({
           ...currentPlayer,
@@ -87,8 +115,8 @@ function Player() {
         }));
       }
 
-      if (currentKey === "a") {
-        // left
+      // left
+      if (currentKey === "KeyA") {
         console.log("left");
 
         setPlayer((currentPlayer) => ({
@@ -100,8 +128,8 @@ function Player() {
         }));
       }
 
-      if (currentKey === "s") {
-        // right
+      // right
+      if (currentKey === "KeyD") {
         console.log("right");
 
         setPlayer((currentPlayer) => ({
@@ -115,15 +143,38 @@ function Player() {
     });
   }
 
+  function handleShooting() {
+    const newBulletId = new Date().getTime();
+
+    if (keysPressed.includes("Space")) {
+      setBullets((currentBullets) => {
+        console.log({ newBulletId });
+        return [
+          ...currentBullets,
+          {
+            id: newBulletId,
+            spawn: {
+              x: player.position.x,
+              y: player.position.y,
+            },
+            // mouseHeading: {
+            //   x: Mouse
+            // }
+          } as unknown as Bullet,
+        ];
+      });
+    }
+  }
+
   function handleAddKey(event: KeyboardEvent) {
-    if (keysPressed.includes(event.key)) return;
-    setKeysPressed((currentKeys) => [...currentKeys, event.key]);
+    if (keysPressed.includes(event.code)) return;
+    setKeysPressed((currentKeys) => [...currentKeys, event.code]);
   }
 
   function handleRemoveKey(event: KeyboardEvent) {
-    if (!keysPressed.includes(event.key)) return;
+    if (!keysPressed.includes(event.code)) return;
     setKeysPressed((currentKeys) =>
-      currentKeys.filter((key) => key !== event.key),
+      currentKeys.filter((code) => code !== event.code)
     );
   }
 
@@ -144,11 +195,138 @@ function Player() {
   );
 }
 
+// NOTE: Bug with bullet deletion (deletes more bullets than desired)
+// I think it's due to the ID assignment, they must all get the same ID
+function Bullet({
+  targetRef,
+  data,
+  setBullets,
+}: {
+  targetRef: MutableRefObject<HTMLDivElement>;
+  data: Bullet;
+  setBullets: Dispatch<SetStateAction<Bullet[]>>;
+}) {
+  const [bulletPosition, setBulletPosition] = useState<BulletPosition>({
+    x: data.spawn.x,
+    y: data.spawn.y,
+  });
+
+  const bulletMovementInterval = useRef<number>(0);
+
+  function handleBulletMovement() {
+    setBulletPosition((currentBulletPosition) => {
+      let newX = currentBulletPosition.x;
+      let newY = currentBulletPosition.y;
+
+      // TODO:
+      // Measure the distance from the bullet to the target
+      // targetRef.current.getBoundingClientRect() -> x, y
+      const { x: targetX, y: targetY } =
+        targetRef.current.getBoundingClientRect();
+      // Pixels / Time = Pixels per update
+      // TargetX - BulletX = xRemaining
+      // xRemaining / 10 = newXIncrement
+
+      const currentTargetX = Math.ceil(targetX);
+      const currentTargetY = Math.ceil(targetY);
+
+      // Return the new position closer to the target
+      if (currentTargetX > currentBulletPosition.x) {
+        const remainingPixels = currentTargetX - currentBulletPosition.x;
+        const pixelsToChange = Math.ceil(remainingPixels / INTERVAL_DELAY);
+        newX += pixelsToChange;
+      }
+
+      if (currentTargetY > currentBulletPosition.y) {
+        const remainingPixels = currentTargetY - currentBulletPosition.y;
+        const pixelsToChange = Math.ceil(remainingPixels / INTERVAL_DELAY);
+        newY += pixelsToChange;
+      }
+
+      if (currentTargetX < currentBulletPosition.x) {
+        const remainingPixels = currentBulletPosition.x - currentTargetX;
+        const pixelsToChange = Math.ceil(remainingPixels / INTERVAL_DELAY);
+        newX -= pixelsToChange;
+      }
+
+      if (currentTargetY < currentBulletPosition.y) {
+        const remainingPixels = currentBulletPosition.y - currentTargetY;
+        const pixelsToChange = Math.ceil(remainingPixels / INTERVAL_DELAY);
+        newY -= pixelsToChange;
+      }
+
+      if (
+        currentTargetX === currentBulletPosition.x &&
+        currentTargetY === currentBulletPosition.y
+      ) {
+        setBullets((currentBullets) => [
+          ...currentBullets.filter((bullet) => bullet.id !== data.id),
+        ]);
+      }
+
+      return { x: newX, y: newY };
+    });
+  }
+
+  useEffect(() => {
+    bulletMovementInterval.current = setInterval(
+      handleBulletMovement,
+      INTERVAL_DELAY
+    );
+
+    return () => {
+      if (bulletMovementInterval.current)
+        clearInterval(bulletMovementInterval.current);
+    };
+  });
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: bulletPosition.y,
+        left: bulletPosition.x,
+      }}
+    >
+      👆
+    </div>
+  );
+}
+
+const TARGET = {
+  y: 750,
+  x: 900,
+};
+
 // TODO: Build a player that we can control with WARS
 function App() {
+  const [bullets, setBullets] = useState<Bullet[]>([]);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  console.log("TOTAL BULLETS: ", bullets.length);
+
   return (
     <>
-      <Player />
+      <Player setBullets={setBullets} />
+
+      {bullets.map((bullet) => {
+        if (!targetRef.current) return null;
+
+        return (
+          <Bullet
+            setBullets={setBullets}
+            targetRef={targetRef as unknown as MutableRefObject<HTMLDivElement>}
+            data={bullet}
+          />
+        );
+      })}
+
+      <div
+        ref={targetRef}
+        style={{ position: "absolute", left: TARGET.x, top: TARGET.y }}
+      >
+        🎯
+      </div>
     </>
   );
 }
